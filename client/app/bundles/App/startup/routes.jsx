b5a0834237e94874df29b7ef4b8ce7f801232d1d
Carla Babadopulos <cacababa@gmail.com>
2016-05-12 20:04:43 +0200

    Revert "Something"

--- client/app/bundles/App/startup/routes.jsx
+++ client/app/bundles/App/startup/routes.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from '../containers/App';
 import AppWidget from '../components/AppWidget';
 import Home from '../containers/Home';
 import Test from '../containers/Test';
 
-module.exports = function (props) {
-  const createElement = function (Component, subProps) {
-    return <Component currentUser={props.currentUser} {...subProps} />
-  };
+export default (
+  <Route path="ui" component={App}>
 
-  return <Router history={browserHistory} createElement={createElement} >
-    <Route path="/" component={App}>
-      <IndexRoute component={AppWidget} />
-      <Route path="test" component={Test} />
-    </Route>
-  </Router>
-}
+    <IndexRoute component={AppWidget} />
+    <Route path="test" component={Test} />
+
+  </Route>
+);
